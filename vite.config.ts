2c691976f24b695a8f20b9ff4ca2daaae026bf4e
Le Chien <lhc021120@gmail.com>
2025-10-24 13:59:55 +0700

    fix deploy web

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react(), tailwindcss(), svgr()],
   server: {
     proxy: {
