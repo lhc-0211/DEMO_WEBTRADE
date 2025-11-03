1d29926a9aa490462739861cbaa57e0888f81136
Le Chien <chienlh@dtnd.vn>
2025-11-03 22:55:17 +0700

    feat(api): get list stockbyid

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import { all } from "redux-saga/effects";
+
 import authSaga from "./slices/auth/saga";
 import authSlice from "./slices/auth/slice";
 import clientSaga from "./slices/client/saga";
 import clientSlice from "./slices/client/slice";
 import placeOrderSaga from "./slices/place-order/saga";
 import placeOrderSlice from "./slices/place-order/slice";
-import priceBoardSaga from "./slices/priceboard/saga";
 import priceBoardSlice from "./slices/priceboard/slice";
-import stockSlice from "./slices/stock/slice.ts";
+import stockSlice from "./slices/stock/slice";
 
 function* rootSaga() {
-  yield all([priceBoardSaga(), authSaga(), clientSaga(), placeOrderSaga()]);
+  yield all([authSaga(), clientSaga(), placeOrderSaga()]);
 }
 
 const sagaMiddleware = createSagaMiddleware();
@@ -26,7 +26,10 @@ export const store = configureStore({
     stock: stockSlice,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({ thunk: false }).concat(sagaMiddleware),
+    getDefaultMiddleware({
+      thunk: true,
+      serializableCheck: false,
+    }).concat(sagaMiddleware),
 });
 
 sagaMiddleware.run(rootSaga);
--- src/store/slices/priceboard/slice.ts
+++ src/store/slices/priceboard/slice.ts
@@ -1,142 +1,26 @@
-import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
-import type {
-  ApiStatus,
-  ChartDataIndex,
-  InfoIndex,
-  topForeignTradedItem,
-  topStockTradedItem,
-} from "../../../types";
-
-// State interface
+import { createSlice } from "@reduxjs/toolkit";
+import type { ApiStatus } from "../../../types";
 
 export interface PriceBoardState {
-  data: {
-    infoIndex: InfoIndex[];
-    chartIndexs: Record<string, ChartDataIndex>;
-    topStockTraded: topStockTradedItem[];
-    topForeignTraded: topForeignTradedItem[];
-  };
-  status: {
-    fetchInfoIndex: ApiStatus;
-    fetchChartIndexs: Record<string, ApiStatus>;
-    fetchTopStockTraded: ApiStatus;
-    fetchTopForeignTraded: ApiStatus;
-  };
+  data: { listStockById: string[] };
+  status: { fetchListStockById: ApiStatus };
 }
 
 const initialState: PriceBoardState = {
-  data: {
-    infoIndex: [],
-    chartIndexs: {},
-    topStockTraded: [],
-    topForeignTraded: [],
-  },
-  status: {
-    fetchInfoIndex: { loading: false, error: null },
-    fetchChartIndexs: {},
-    fetchTopStockTraded: { loading: false, error: null },
-    fetchTopForeignTraded: { loading: false, error: null },
-  },
+  data: { listStockById: [] },
+  status: { fetchListStockById: { loading: false, error: null } },
 };
 
 const priceBoardSlice = createSlice({
   name: "priceBoard",
   initialState,
   reducers: {
-    // Thông tin Index
-    fetchInfoIndexRequest(state) {
-      state.status.fetchInfoIndex = { loading: true, error: null };
-      state.data.infoIndex = [];
-    },
-    fetchInfoIndexSuccess(state, action: PayloadAction<InfoIndex[]>) {
-      state.status.fetchInfoIndex = { loading: false, error: null };
-      state.data.infoIndex = action.payload;
-    },
-    fetchInfoIndexFailure(state, action: PayloadAction<string>) {
-      state.status.fetchInfoIndex = { loading: false, error: action.payload };
-      state.data.infoIndex = [];
-    },
-
-    // Biểu đồ Index
-    fetchChartIndexRequest(state, action: PayloadAction<string>) {
-      const id = action.payload;
-
-      if (!state.status.fetchChartIndexs[id]) {
-        state.status.fetchChartIndexs[id] = { loading: false, error: null };
-      }
-      state.status.fetchChartIndexs[id].loading = true;
-      state.status.fetchChartIndexs[id].error = null;
-      state.data.chartIndexs[id] = { id, data: [] };
-    },
-    fetchChartIndexSuccess(state, action: PayloadAction<ChartDataIndex>) {
-      const id = action.payload.id;
-      state.status.fetchChartIndexs[id] = { loading: false, error: null };
-      state.data.chartIndexs[id] = action.payload;
-    },
-    fetchChartIndexFailure(state, action: PayloadAction<string>) {
-      const id = action.payload;
-      state.status.fetchChartIndexs[id] = {
-        loading: false,
-        error: action.payload,
-      };
-      state.data.chartIndexs[id] = { id, data: [] };
-    },
-
-    // Thông tin Top KL giao dịch trong ngày
-    fetchTopStockTradedRequest(state, action: PayloadAction<string>) {
-      state.status.fetchTopStockTraded = { loading: true, error: null };
-      state.data.topStockTraded = [];
-    },
-    fetchTopStockTradedSuccess(
-      state,
-      action: PayloadAction<topStockTradedItem[]>
-    ) {
-      state.status.fetchTopStockTraded = { loading: false, error: null };
-      state.data.topStockTraded = action.payload;
-    },
-    fetchTopStockTradedFailure(state, action: PayloadAction<string>) {
-      state.status.fetchTopStockTraded = {
-        loading: false,
-        error: action.payload,
-      };
-      state.data.topStockTraded = [];
-    },
-
-    // Thông tin Top KL mua bán nước ngoài
-    fetchTopForeignTradedRequest(state, action: PayloadAction<string>) {
-      state.status.fetchTopForeignTraded = { loading: true, error: null };
-      state.data.topForeignTraded = [];
-    },
-    fetchTopForeignTradedSuccess(
-      state,
-      action: PayloadAction<topForeignTradedItem[]>
-    ) {
-      state.status.fetchTopForeignTraded = { loading: false, error: null };
-      state.data.topForeignTraded = action.payload;
-    },
-    fetchTopForeignTradedFailure(state, action: PayloadAction<string>) {
-      state.status.fetchTopForeignTraded = {
-        loading: false,
-        error: action.payload,
-      };
-      state.data.topForeignTraded = [];
+    setListStockByIdFromCache(state, action) {
+      state.data.listStockById = action.payload;
+      state.status.fetchListStockById = { loading: false, error: null };
     },
   },
 });
 
-export const {
-  fetchInfoIndexRequest,
-  fetchInfoIndexSuccess,
-  fetchInfoIndexFailure,
-  fetchChartIndexRequest,
-  fetchChartIndexSuccess,
-  fetchChartIndexFailure,
-  fetchTopStockTradedRequest,
-  fetchTopStockTradedSuccess,
-  fetchTopStockTradedFailure,
-  fetchTopForeignTradedRequest,
-  fetchTopForeignTradedSuccess,
-  fetchTopForeignTradedFailure,
-} = priceBoardSlice.actions;
-
+export const { setListStockByIdFromCache } = priceBoardSlice.actions;
 export default priceBoardSlice.reducer;
